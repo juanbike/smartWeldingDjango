be1caa3a3191754bed32d11446453e190f93672b
juanbike <juancarlos.arteaga1@gmail.com>
2023-06-04 05:13:12 -0400

    creacion de los filtros (menu desplegable) para el inspector y soldador

--- smartwelding/views.py
+++ smartwelding/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from smartwelding.models import Soldador, Inspector
 
+""""
 def miplantilla(request):
     titulo = 'Bienvenidos a mi vista'
     contenido = 'Esta es una plantilla en Django'
     return render(request, 'pages/miplantilla.html', {'titulo': titulo, 'contenido': contenido})
+"""
+
+
+def miplantilla(request):
+    inspectores = Inspector.objects.all()
+    soldadores = Soldador.objects.all()
+    print(inspectores)
+    return render(request, 'pages/miplantilla.html', {'inspectores': inspectores, 'soldadores': soldadores })
