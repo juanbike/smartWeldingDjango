4e05ea4b70eea2fe485cb1faeedf3b44529e287f
juanbike <juancarlos.arteaga1@gmail.com>
2023-06-05 11:17:23 -0400

    Merge branch 'master' of https://github.com/juanbike/smartWeldingDjango

--- smartwelding/models.py
+++ smartwelding/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
 
 
@@ -247,8 +247,11 @@ class Inspector(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     telefono = models.CharField(max_length=20)
-    telefono2 = models.CharField(max_length=20)
+    telefono2 = models.CharField(max_length=20, null=False, blank=True)
     creado = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido} {self.telefono}"
+        return f"{self.nombre} {self.apellido}  {self.telefono} {'|'} {self.telefono}2"
+
+
+#
