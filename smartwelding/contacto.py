696a34c211260ee076dfdeee91fac825eaf2d1ae
juanbike <juancarlos.arteaga1@gmail.com>
2023-06-07 14:57:19 -0400

    validacion de un campo y mostrar info de una tabla en un select del formulario

--- smartwelding/contacto.py
+++ smartwelding/contacto.py
@@ -2,17 +2,22 @@ from django import forms
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from requests import request
+from django.core.exceptions import ValidationError
+from .models import Linea
+
+# creamos la clase ContactoForm tiene los metodos y propiedades para el formulario de contacto
 
 
 class ContactoForm(forms.Form):
-    sunombre = forms.CharField(max_length=100, label='Su nombre')
+    sunombre = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Ingese su nombre', 'maxlenght': '12'}))
     email = forms.EmailField(required=False, label='Su correo')
+    subject = forms.CharField(max_length=100)
+    linea = forms.ModelChoiceField(queryset=Linea.objects.all(), empty_label='Seleccione una linea') 
+    mensaje = forms.CharField(widget=forms.Textarea ,label='Mensaje', min_length=10)
 
 
-subject = forms.CharField(max_length=100)
-message = forms.CharField(widget=forms.Textarea)
-
-
+# Creamos una funcion para manejar las solicitudes get y post desde el cliente
 def contacto(request):
     submitted = False
     if request.method == 'POST':
@@ -30,3 +35,11 @@ def contacto(request):
                   'pages/contacto.html',
                   {'form': form, 'submitted': submitted}
                   )
+
+
+def clean_mensaje(self):
+    mensaje = self.cleaned_data['mensaje']
+    if len(mensaje) < 10:
+        raise forms.ValidationError(
+            "El mensaje debe tener al menos 10 caracteres.")
+    return mensaje
--- smartwelding/urls.py
+++ smartwelding/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 #from smartwelding.views import miplantilla //asi tambien funciona
 from .views import *
+from . contacto import contacto
 urlpatterns = [
     path("juntas/", include("juntas.urls")),
     path('admin/', admin.site.urls),
     path("plantilla/", view=miplantilla, ),
-    
+    path('contacto/', contacto, name='contacto')
 ]
